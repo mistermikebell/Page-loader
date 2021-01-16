fa1f82bfa764d586c05d01611a7dd67052a09ce0
mistermikebell <jokerbell7@gmail.com>
2021-01-16 19:38:46 +0300

    correcting mistakes v1.2

--- tests/test_page_loader.py
+++ tests/test_page_loader.py
@@ -40,11 +40,12 @@ def test_load_html(requests_mock):
         expected_path = abspath(join(tmpdir, 'www-site-com.html'))
         assert download(URL, tmpdir) == expected_path
         result_files = os.listdir(tmpdir)
-        expected_files = os.listdir('fixtures/result')
+        expected_files = os.listdir('./tests/fixtures/fixtures/result')
         assert result_files == expected_files
         result_dir = get_dir(tmpdir)
         result_files_list = os.listdir(result_dir)
-        expected_files_list = os.listdir('./tests/fixtures/result/www-site-com_files')
+        expected_files_list = os.listdir('./tests/fixtures/result/'
+                                         'www-site-com_files')
         assert result_files_list == expected_files_list
 
 
