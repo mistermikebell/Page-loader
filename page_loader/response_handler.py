d4e245d068e6afcb8a274445ac215b469c1afe80
mistermikebell <jokerbell7@gmail.com>
2021-01-18 22:08:49 +0300

    correcting mistakes v1.9.6.3

--- page_loader/response_handler.py
+++ page_loader/response_handler.py
@@ -9,14 +9,13 @@ from urllib3.exceptions import (MaxRetryError, NewConnectionError,
 def try_load_url(url):
     try:
         response = requests.get(url)
-        '''
-        except (NewConnectionError, MaxRetryError, ResponseError,
-                ConnectTimeoutError, ConnectionRefusedError, ConnectionError):
-            raise ConnectionError('ConnectionError')
-        except InvalidSchema:
-            raise InvalidSchema("ConnectionError: Invalid url")
-        try:
-        '''
+    except (NewConnectionError, MaxRetryError, ResponseError,
+            ConnectTimeoutError, ConnectionRefusedError, ConnectionError):
+        logging.error('ConnectionError')
+        raise ConnectionError('ConnectionError')
+    except InvalidSchema:
+        raise InvalidSchema("ConnectionError: Invalid url")
+    try:
         response.raise_for_status()
     except HTTPError:
         logging.error(f'Status code is {response.status_code}')
