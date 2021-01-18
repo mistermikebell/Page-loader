739bd63895054e3977269cb6186e1abfd232361f
mistermikebell <jokerbell7@gmail.com>
2021-01-18 14:27:36 +0300

    correcting mistakes v1.9.3

--- page_loader/logging.py
+++ page_loader/logging.py
@@ -3,7 +3,7 @@ import logging
 FORMAT = '%(asctime)s %(name)s %(levelname)s:%(message)s'
 
 
-def setup(log_level=2, log_file="no file"):
+def setup(log_level=3, log_file="no file"):
     if log_file == "no file":
         return None
     logging.basicConfig(filename=log_file,
